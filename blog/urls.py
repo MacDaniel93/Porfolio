1256b66dca59a859494af0bae65341bba3d841fe
MacDaniel93 <152479373+MacDaniel93@users.noreply.github.com>
2023-11-30 04:53:51 -0500

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import render_posts,post_detail
+
 
 app_name = 'blog'
 
 urlpatterns = [
-    path("", views.renderPosts, name="posts"),
-    path("<int:post_id>", views.post_detail, name="post_detail"),
-    
+    path('', render_posts, name='posts'), 
+    path('<int:post_id>', post_detail, name='post_detail')
 ]
