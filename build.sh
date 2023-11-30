bfe8a918b281c716211aed266d90a4d62d0d3c5b
MacDaniel93 <152479373+MacDaniel93@users.noreply.github.com>
2023-11-30 04:57:07 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,5 @@ set -o errexit
 # poetry install 
 pip install -r requirements.txt
 
-DJANGO_SUPERUSER_PASSWORD=12345 python manage.py createsuperuser --no-input --username=jean  --email=admin@example.com
-
 python manage.py collectstatic --no-input
 python manage.py migrate
